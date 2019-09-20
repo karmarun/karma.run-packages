36ce4dc96ab334f541de79a90b68e108dd765ebe
Gabriel DU DA <gabriel@Gabriels-MBP-15.Frachtraum>
2019-09-20 16:38:38 +0200

    Add styling for input field

--- packages/cms-kit/src/atoms/input.tsx
+++ packages/cms-kit/src/atoms/input.tsx
@@ -2,17 +2,52 @@ import React from 'react'
 import {IconType, Icon} from './icon'
 import {cssRuleWithTheme, useThemeStyle, ThemeContext} from '../style/themeContext'
 import {joinClassNames} from '@karma.run/react'
+import {pxToRem, FontSize, TransitionDuration} from '../style/helpers'
 
 export interface InputStyleProps {
   hasError: boolean
 }
 
+export const InputFieldStyle = cssRuleWithTheme(({theme}) => ({}))
+
 export const InputStyle = cssRuleWithTheme(({theme}) => ({
-  // todo
+  position: 'relative',
+  marginTop: '1.6rem',
+
+  '& input': {
+    width: '100%',
+    border: 'none',
+    borderBottom: `1px solid ${theme.colors.gray}`,
+    transition: `border-color ease-in ${TransitionDuration.Slow}`,
+
+    fontSize: pxToRem(FontSize.Medium),
+
+    '::placeholder': {
+      color: theme.colors.gray
+    },
+
+    ':focus': {
+      outline: 'none',
+      borderColor: theme.colors.action
+    },
+
+    ':placeholder-shown + label': {
+      opacity: '0',
+      transform: 'translateY(30%)'
+    }
+  }
 }))
 
 const LabelStyle = cssRuleWithTheme<InputStyleProps>(({hasError, theme}) => ({
-  color: hasError ? theme.colors.alert : theme.colors.action
+  color: hasError ? theme.colors.alert : theme.colors.action,
+  position: 'absolute',
+  top: '-1.6rem',
+  left: '0',
+  fontSize: pxToRem(FontSize.Small),
+  opacity: '1',
+  transform: 'translateY(0%)',
+  transition: 'transform ease-in-out, opacity ease-in-out, color ease-in-out',
+  transitionDuration: TransitionDuration.Fast
 }))
 
 const DescriptionStyle = cssRuleWithTheme<InputStyleProps>(({hasError, theme}) => ({
@@ -53,9 +88,8 @@ export function Input({
   )
 
   return (
-    <div className={joinClassNames(css(InputStyle), className)}>
-      <div className={css(LabelStyle)}>{label}</div>
-      <div>
+    <div className={joinClassNames(css(InputFieldStyle), className)}>
+      <div className={joinClassNames(css(InputStyle))}>
         {icon ? (
           <>
             <Icon type={icon} /> {Input}
@@ -63,7 +97,9 @@ export function Input({
         ) : (
           Input
         )}
+        <label className={css(LabelStyle)}>{label}</label>
       </div>
+
       <div className={css(DescriptionStyle)}>{errorDescription && description}</div>
     </div>
   )
