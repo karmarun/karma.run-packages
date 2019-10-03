5d498458837d0b7b302c790bc2b00c1b5c558c97
Fiona Nueesch <fionanueesch@fionas-mbp.lan>
2019-10-03 16:49:32 +0200

    Refactor into fileDropZone and imageUpload - add buttonBar

--- packages/cms-kit/src/molecules/imageUpload.stories.tsx
+++ packages/cms-kit/src/molecules/imageUpload.stories.tsx
@@ -1,49 +1,46 @@
 import React, {useState} from 'react'
 
 import {centerLayoutDecorator} from '../.storybook/decorators'
-import {ImageUpload} from './imageUpload'
+import {ImageUpload, ImageUploadThumb} from './imageUpload'
 
 export default {
   component: ImageUpload,
-  title: 'Molecules|ImageUpload',
+  title: 'Molecules|ImagesUpload',
   decorators: [centerLayoutDecorator(0.6)]
 }
 
 export const Standard = () => {
-  const [files, setFiles] = useState([])
-
-  function addFiles(fileList: FileList) {
-    let newFiles = files
-    for (let i = 0; i < fileList.length; i++) {
-      newFiles.push(fileList.item(i))
-    }
-    setFiles(newFiles)
-  }
-
-  console.log(files)
-
-  return <ImageUpload onDrop={addFiles} images={[]} isProcessing={false} onDelete={id => {}} />
+  return <ImageUpload onUpload={() => {}} />
 }
 
-export const Upload = () => (
-  <ImageUpload
-    onDrop={fileList => {}}
-    images={mockThumbImages}
-    isProcessing={false}
-    onDelete={id => {}}
-  />
-)
+export const ImageThumb = () => {
+  return (
+    <ImageUploadThumb
+      id={mockThumbImage.id}
+      src={mockThumbImage.src}
+      name={mockThumbImage.name}
+      size={mockThumbImage.size}
+      onDelete={() => {}}
+    />
+  )
+}
 
-export const InProcess = () => (
-  <ImageUpload
-    onDrop={fileList => {}}
-    images={mockThumbImages}
-    isProcessing={true}
-    onDelete={id => {}}
-  />
-)
+export const ImageThumbUploading = () => {
+  return (
+    <ImageUploadThumb
+      id={mockThumbImage.id}
+      src={mockThumbImage.src}
+      name={mockThumbImage.name}
+      size={mockThumbImage.size}
+      isLoading={true}
+      onDelete={() => {}}
+    />
+  )
+}
 
-const mockThumbImages = [
-  {id: 'banana', src: 'https://dummyimage.com/100x68/000/fff', size: '1.1MB', name: 'Banana.jpg'},
-  {id: 'flower', src: 'https://dummyimage.com/100x68/ba37ba/fff', size: '1.5MB', name: 'flower.jpg'}
-]
+const mockThumbImage = {
+  id: 0,
+  src: 'https://dummyimage.com/100x68/000/fff',
+  size: '1.1MB',
+  name: 'Banana.jpg'
+}
--- packages/cms-kit/src/molecules/imageUpload.tsx
+++ packages/cms-kit/src/molecules/imageUpload.tsx
@@ -1,188 +1,112 @@
 import React, {useState} from 'react'
 
-import {IconType, Icon, IconScale} from '../atoms/icon'
-import {cssRuleWithTheme, useThemeStyle} from '../style/themeContext'
+import {FileDropZone} from './fileDropZone'
 import {OptionButtonSmall} from '../atoms/optionButtonSmall'
-import {pxToRem, FontSize} from '../style/helpers'
-import {cssRule} from '@karma.run/react'
+import {IconType} from '..'
+import {ProgressBar} from '../atoms/progressBar'
+import {ButtonBar} from '../atoms/buttonBar'
+import {cssRule, useStyle} from '@karma.run/react'
+import {Icon} from '../atoms/icon'
 
-const ImageUploadStyle = cssRuleWithTheme<{dragging: boolean; inProcess: boolean}>(
-  ({dragging, inProcess, theme}) => ({
-    display: 'flex',
-    borderRadius: pxToRem(3),
-    border: dragging
-      ? `1px dashed ${theme.colors.action}`
-      : `1px dashed ${theme.colors.actionDark}`,
-    position: 'relative',
-    cursor: inProcess ? 'not-allowed' : 'default'
-  })
-)
-
-const ThumbStyle = cssRuleWithTheme<{inProcess: boolean}>(({inProcess}) => ({
-  opacity: inProcess ? 0.4 : 1
-}))
-
-const DragOverStyle = cssRule({
-  position: 'absolute',
-  top: 0,
-  left: 0,
-  width: '100%',
-  height: '100%',
-  backgroundColor: 'rgba(255, 255, 255, 0.7)'
-})
-
-export interface ImageUploadProps {
-  readonly images: ImageUploadThumbProps[]
-  readonly isProcessing: boolean
-  onDelete(id: string): void
-  onDrop(files: FileList): void
+enum UploadState {
+  Empty = 'empty',
+  ReadyToUpload = 'ready',
+  Uploading = 'uploading'
 }
 
-export function ImageUpload({
-  images,
-  isProcessing,
-  onDelete: onDeleteImage,
-  onDrop
-}: ImageUploadProps) {
-  const [{dragging, dragCount}, setDragging] = useState({dragging: false, dragCount: 0})
-  const {css} = useThemeStyle({dragging: dragging, inProcess: isProcessing})
+export interface ImageUploadProps {
+  // upload progress in percentage
+  readonly uploadProgress?: number
+  onUpload(files: File[]): void
+  onUploadCancel?(): void
+}
 
-  function handleDrag(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault()
-    e.stopPropagation()
-  }
+export function ImageUpload({uploadProgress, onUpload, onUploadCancel}: ImageUploadProps) {
+  const [images, setImages] = useState<File[]>([])
 
-  function handleDragIn(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault()
-    e.stopPropagation()
-    setDragging({dragging: true, dragCount: dragCount + 1})
-  }
+  function addImages(fileList: FileList) {
+    let newImages = [...images]
 
-  function handleDragOut(e: React.DragEvent<HTMLDivElement>) {
-    e.preventDefault()
-    e.stopPropagation()
-
-    if (dragCount - 1 == 0) {
-      setDragging({dragging: false, dragCount: dragCount - 1})
-    } else {
-      setDragging({dragging: dragging, dragCount: dragCount - 1})
+    for (let i = 0; i < fileList.length; i++) {
+      if (fileList.item(i) != null) {
+        newImages.push(fileList.item(i)!)
+      }
     }
+    setImages(newImages)
   }
 
-  function handleDrop(event: React.DragEvent<HTMLDivElement>) {
-    event.preventDefault()
-    event.stopPropagation()
-
-    setDragging({dragging: false, dragCount: 0})
-
-    if (event.dataTransfer.files && event.dataTransfer.files.length > 0) {
-      onDrop(event.dataTransfer.files)
-      event.dataTransfer.clearData()
-    }
+  function removeImage(idx: number) {
+    let newImages = [...images]
+    newImages.splice(idx, 1)
+    setImages(newImages)
   }
 
-  function getState() {
-    if (images.length == 0) return UploadState.Empty
-    else if (isProcessing) return UploadState.InProcess
-    else return UploadState.Upload
+  function removeAll() {
+    setImages([])
   }
 
   return (
-    <div
-      className={css(ImageUploadStyle)}
-      onDrop={!isProcessing ? handleDrop : undefined}
-      onDragOver={!isProcessing ? handleDrag : undefined}
-      onDragEnter={!isProcessing ? handleDragIn : undefined}
-      onDragLeave={!isProcessing ? handleDragOut : undefined}>
-      <ImageUploadIcon state={getState()} />
+    <FileDropZone onDrop={addImages} showPlaceholder={images.length == 0}>
       {images.map((img, idx) => (
-        <div key={idx} className={css(ThumbStyle)}>
-          <OptionButtonSmall
-            disabled={isProcessing}
-            icon={IconType.Close}
-            onClick={() => onDeleteImage(img.id)}
+        <div key={idx}>
+          <ImageUploadThumb
+            id={idx}
+            src={URL.createObjectURL(img)}
+            size={`${img.size}`}
+            name={img.name}
+            onDelete={removeImage}
+            isLoading={uploadProgress != undefined}
           />
-          <ImageUploadThumb id={img.id} src={img.src} size={img.size} name={img.name} />
         </div>
       ))}
 
-      {dragging && (
-        <div className={css(DragOverStyle)}>
-          <ImageUploadIcon state={UploadState.DragOver} />
+      {images.length > 0 && (
+        <div>
+          {uploadProgress ? (
+            <ProgressBar progress={uploadProgress} onCancel={onUploadCancel} />
+          ) : (
+            <ButtonBar
+              cancelLabel={'Cancel'}
+              onCancel={removeAll}
+              confirmLabel={'Upload'}
+              onConfirm={() => onUpload(images)}
+            />
+          )}
         </div>
       )}
-    </div>
+    </FileDropZone>
   )
 }
 
 /**
  *
- * upload icon and info text component
+ * Image Upload Thumb
  */
-const UploadInfoStyle = cssRuleWithTheme<{inProcess: boolean}>(({inProcess, theme}) => ({
-  color: inProcess ? theme.colors.primary : theme.colors.action,
-  fill: inProcess ? theme.colors.primary : theme.colors.action,
+const ImageUploadThumbStyle = cssRule({})
 
-  fontSize: pxToRem(FontSize.Small),
-  textAlign: 'center'
-}))
-
-const UploadInfoLabelStyle = cssRuleWithTheme<{inProcess: boolean}>(({inProcess, theme}) => ({}))
-
-export enum UploadState {
-  Empty,
-  Upload,
-  DragOver,
-  InProcess
-}
-
-export interface ImageUploadIconProps {
-  state: UploadState
-}
-
-export function ImageUploadIcon({state}: ImageUploadIconProps) {
-  const isInProcess = state == UploadState.InProcess
-  const {css} = useThemeStyle({inProcess: isInProcess})
-
-  function getInfoText() {
-    switch (state) {
-      case UploadState.Empty:
-        return 'drop image here or click to upload'
-
-      case UploadState.Upload:
-        return 'upload all'
-
-      case UploadState.InProcess:
-        return 'in process'
-
-      case UploadState.DragOver:
-        return 'drop here'
-    }
-  }
-
-  return (
-    <div className={css(UploadInfoStyle)}>
-      <Icon type={isInProcess ? IconType.Created : IconType.Upload} scale={IconScale.Double} />
-      <div className={css(UploadInfoLabelStyle)}>{getInfoText()}</div>
-    </div>
-  )
-}
-
-/**
- *
- * image thumb
- */
 export interface ImageUploadThumbProps {
-  readonly id: string
+  readonly id: number
   readonly src: string
-  readonly size: string
-  readonly name: string
+  readonly size?: string
+  readonly name?: string
+  readonly isLoading?: boolean
+  onDelete(id: number): void
 }
 
-export function ImageUploadThumb({src, size, name}: ImageUploadThumbProps) {
+export function ImageUploadThumb({
+  id,
+  src,
+  size,
+  name,
+  isLoading,
+  onDelete
+}: ImageUploadThumbProps) {
+  const {css} = useStyle()
   return (
-    <div>
-      <img src={src} />
+    <div className={css(ImageUploadThumbStyle)}>
+      {!isLoading && <OptionButtonSmall icon={IconType.Delete} onClick={e => onDelete(id)} />}
+      {isLoading && <Icon type={IconType.Upload} />}
+      <img src={src} width={210} height={140} />
       <div>{size}</div>
       <div>{name}</div>
     </div>
