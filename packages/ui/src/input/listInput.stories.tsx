7238f9130b5cb2a6d456a209dec3b308ec6d6f2a
Tobias Langhart <tobias.langhart@dudagroup.com>
2020-02-17 17:50:01 +0100

    add list input wip

---
+++ packages/ui/src/input/listInput.stories.tsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+
+import {centerLayoutDecorator} from '../.storybook/decorators'
+import {ListField, ListValue} from './listInput'
+import {TextInput} from './textInput'
+
+export default {
+  component: ListField,
+  title: 'Input|ListInput',
+  decorators: [centerLayoutDecorator()]
+}
+
+export const Default = () => {
+  const [values, setValues] = useState<ListValue[]>([
+    // const [values, setValues] = useState<ListValue<string>[]>([
+    {id: '1', value: 'Hello'},
+    {id: '2', value: 'World'},
+    {id: '3', value: 'Test'}
+  ])
+
+  return (
+    <ListField value={values} onChange={setValues} defaultValue={''}>
+      {props => (
+        <TextInput
+          label="Label"
+          value={props.value}
+          // disabled={disabled}
+          onChange={e => {
+            setValues(e.currentTarget.value)
+          }}
+        />
+      )}
+    </ListField>
+  )
+}
+
+export const Disabled = () => {
+  const [values, setValues] = useState<ListValue[]>([
+    // const [values, setValues] = useState<ListValue<string>[]>([
+    {id: '1', value: 'Hello'},
+    {id: '2', value: 'World'},
+    {id: '3', value: 'Test'}
+  ])
+
+  return (
+    <ListField value={values} onChange={setValues} defaultValue={''} disabled>
+      {props => (
+        <TextInput
+          label="Label"
+          value={props.value}
+          // disabled={disabled}
+          onChange={e => {
+            setValues(e.currentTarget.value)
+          }}
+        />
+      )}
+    </ListField>
+  )
+}
